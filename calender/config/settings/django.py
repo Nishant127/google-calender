74aefb2b1187cfd50cec7a85be10b3bfefe0adce
Nishant127 <handgenishant@gmail.com>
2023-05-27 16:46:39 +0530

    NH|Create .env file to store secrets.

--- calender/config/settings/django.py
+++ calender/config/settings/django.py
@@ -28,7 +28,7 @@ env = environ.Env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-79bzi8-l-(nqlks&q78p&x8_!og#mja^@$t2rdy66egxz-h-%k"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
